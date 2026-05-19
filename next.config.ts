d3a49ae5fec7f496f0ca02b0f1f40d1faa152037
ShroukMagdy3 <shorokmagdy15@gmail.com>
2026-05-20 02:47:25 +0300

    add:login with google

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "*.fbcdn.net",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
   },
 };
--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Facebook from "next-auth/providers/facebook";
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   secret: process.env.AUTH_SECRET,
@@ -7,6 +8,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     Facebook({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   trustHost: true,
